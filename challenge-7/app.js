4a5c877bffbe3c25303841b9bbdc7dcc9c63d371
varunchimurkar <varunchimurkar96@gmail.com>
2025-04-18 08:17:58 +0530

    Challenge-7 ✅

--- challenge-7/app.js
+++ challenge-7/app.js
@@ -1,3 +1,23 @@
 /**
  * Write your challenge solution here
  */
+
+const btn = document.querySelectorAll('.accordion-button')
+
+
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        const parentElement = button.parentElement
+        const open = parentElement.classList.contains('active')
+
+        btn.forEach(button => {
+            button.parentElement.classList.remove('active')
+        })
+
+        if (!open) {
+            parentElement.classList.add('active')
+        }
+
+    })
+
+})
